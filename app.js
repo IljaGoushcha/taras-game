9e9be923e6389154ecc0a7033c0f4ab906e8cf02
IljaGoushcha <iljagou@yahoo.com>
2015-05-12 16:18:41 -0400

    added dificulty level

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function moveDoggy() {
   console.log("inside moveDoggy");
+  console.log("difficulty: " + $('select').val());
+
   var left = Math.random()*500;
   var top = Math.random()*500;
   console.log("left: " + left + "; top: " + top);
@@ -7,6 +9,30 @@ function moveDoggy() {
     "left": left,
     "top": top,
   });
+  if ($('select').val() == "difficult") {
+    var left = Math.random()*500;
+    var top = Math.random()*500;
+    console.log("left: " + left + "; top: " + top);
+    $('.inner-box').animate({
+      "left": left,
+      "top": top,
+    });
+  } else if ($('select').val() == "extreme") {
+    var left = Math.random()*500;
+    var top = Math.random()*500;
+    console.log("left: " + left + "; top: " + top);
+    $('.inner-box').animate({
+      "left": left,
+      "top": top,
+    });
+    var left = Math.random()*500;
+    var top = Math.random()*500;
+    console.log("left: " + left + "; top: " + top);
+    $('.inner-box').animate({
+      "left": left,
+      "top": top,
+    });
+  }
 }
 
 $(document).ready(function() {
