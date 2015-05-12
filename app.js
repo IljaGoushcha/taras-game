b3505bbfc00fe58afecaf23fe37ef6bff0e1c525
IljaGoushcha <iljagou@yahoo.com>
2015-05-12 15:42:29 -0400

    added nice doggy

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var offset = 0
-
-function moveSymbol() {
-  console.log("inside moveSymbol");
-  offset = offset + 40;
-  $('p').css({"padding-left": offset});
+function moveDoggy() {
+  console.log("inside moveDoggy");
+  var left = Math.random()*500;
+  var top = Math.random()*500;
+  console.log("left: " + left + "; top: " + top);
+  $('.inner-box').animate({
+    "left": left,
+    "top": top,
+  });
 }
 
 $(document).ready(function() {
 
   console.log( "ready!" );
 
-  $('p').on('mouseover', moveSymbol);
-  $('p').on('mouseleave', moveSymbol);
+  $('.inner-box').on('mouseover', moveDoggy);
+  $('.inner-box').on('mouseleave', moveDoggy);
 
 });
